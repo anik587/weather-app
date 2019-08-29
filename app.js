97424ba2ab4b12050307cab285099108fb022fad
anik587 <anik587@gmail.com>
2019-08-29 14:27:27 +0600

    error handler introduced

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ request({
     json: true
 }, 
  (error, response) =>{
-     const data = response.body;
-     console.log(data.daily.data[0].summary);
-     console.log(data.daily.data[0].precipType);
+     if(error){
+         console.log('Unable to connect weather service')
+     }else if(response.body.error){
+        console.log('Unable to find weather data');
+     }else{
+         const data = response.body;
+         console.log(data.daily.data[0].summary);
+         console.log(data.daily.data[0].precipType);
+
+     }
 });
 
--- getPosition.js
+++ getPosition.js
@@ -1,9 +1,18 @@
 const request = require('request');
-const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5pazU4NyIsImEiOiJjanp3ZWJjaGgwYXVvM2xvMTJ0bHhoN21oIn0.VRvnGkVOjUKRxKdl3Yz6_A&limit=1";
+const geoCodeUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?11access_token=pk.eyJ1IjoiYW5pazU4NyIsImEiOiJjanp3ZWJjaGgwYXVvM2xvMTJ0bHhoN21oIn0.VRvnGkVOjUKRxKdl3Yz6_A&limit=1";
 request({
-    url: url,
+    url: geoCodeUrl,
     json: true,
 }, (error, response)=>{
-        console.log(`latitude : ${response.body.features[0].center[0]}`);
-        console.log(`longitude : ${response.body.features[0].center[1]}`);
+        if(error){
+            console.log('Unable to connect location service')    
+        } if (response.body.features === undefined) {
+            console.log('Unable to fetch location');
+        } else if (response.body.features.length === 0){
+            console.log('Unknown Location');
+        }else{
+            console.log(`latitude : ${response.body.features[0].center[0]}`);
+            console.log(`longitude : ${response.body.features[0].center[1]}`);
+
+        }
     })
