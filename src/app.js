2da385bc7f32bacf2befb6b79c5c7a44582270fc
anik587 <anik587@gmail.com>
2019-09-23 08:01:57 +0600

    handlebar, express added, template(view+partial) added, server started

--- src/app.js
+++ src/app.js
@@ -1,2 +1,38 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
 const { getLocation,getWeather}  = require('./utils/index');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const viewPath = path.join(__dirname, '../src/templates/views');
+const partialPath = path.join(__dirname, '../src/templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
+hbs.registerPartials(partialPath);
+
+
+app.get(['/'], (req, res)=>{
+    res.render('index');
+})
+
+
+app.get('/help', (req, res) => {
+    res.render('help');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('*', (req, res) => {
+    res.render('404');
+})
+
+
+app.listen(PORT, ()=>{
+    console.log('weather app started at port'+ PORT)
+})
